34d4fe9b8cbc3fb9e63bf272124cbe566336123a
Joachim Ante <joe@unity3d.com>
2019-03-24 09:53:51 -0700

    Basic scatter

--- Assets/ProceduralScatter.cs
+++ Assets/ProceduralScatter.cs
@@ -2,9 +2,9 @@
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
-using Unity.Assertions;
 using Unity.Transforms;
 using UnityEngine;
+using UnityEngine.Assertions;
 using Random = Unity.Mathematics.Random;
 
 public struct ProceduralScatterPrefab : IBufferElementData
@@ -58,16 +58,19 @@ class ScatterStreamingSystem : JobComponentSystem
         {
             //@TODO: Copy reinterpreted data out is a bit awkward... 
             var tempPrefabs = Transaction.GetBuffer<ProceduralScatterPrefab>(ScatterPrefab).Reinterpret<Entity>().AsNativeArray();
-            var prefabs = new NativeArray<Entity>(tempPrefabs.Length, Allocator.Temp);
-            prefabs.CopyFrom(tempPrefabs);
+            var prefabs = new NativeArray<Entity>(tempPrefabs, Allocator.Temp);
 
             //@TODO: Sort by prefab index and instantiate in batch
-            
+
             for (int i = 0; i != InstanceData.Length; i++)
             {
                 var instanceData = InstanceData[i];
                 var instance = Transaction.Instantiate(prefabs[instanceData.PrefabIndex]);
-                Transaction.SetComponentData(instance, new Translation { Value = instanceData.Position});
+                Transaction.SetComponentData(instance, new Translation {Value = instanceData.Position});
+
+                if (!Transaction.HasComponent(instance, ComponentType.ReadWrite<Scale>()))
+                    Transaction.AddComponent(instance, ComponentType.ReadWrite<Scale>());
+                Transaction.SetComponentData(instance, new Scale {Value = instanceData.Scale});
             }
         }
     }
@@ -75,11 +78,13 @@ class ScatterStreamingSystem : JobComponentSystem
     Entity m_SrcScatterPrefab;
     Entity m_StreamingWorldScatterPrefab;
     ComponentGroup m_ScatterPrefabQuery;
+    ComponentGroup m_InstancesQuery;
 
     void PrepareScatterPrefab(Entity scatterPrefab)
     {
         if (scatterPrefab == m_SrcScatterPrefab)
             return;
+
         m_SrcScatterPrefab = scatterPrefab;
 
         // Deep copy all of referenced prefabs
@@ -101,7 +106,6 @@ class ScatterStreamingSystem : JobComponentSystem
             var linkedEntities = EntityManager.GetBuffer<LinkedEntityGroup>(dstPrefabs[i]);
             for (int j = 0; j != linkedEntities.Length; j++)
             {
-                Debug.Log(linkedEntities[j].Value);
                 commands.AddComponent(linkedEntities[j].Value, default(TempProceduralScatterPrefabTag));
                 commands.AddComponent(linkedEntities[j].Value, default(Prefab));
             }
@@ -114,15 +118,16 @@ class ScatterStreamingSystem : JobComponentSystem
         m_StreamingWorld.EntityManager.MoveEntitiesFrom(EntityManager, m_ScatterPrefabQuery, entityRemapping);
 
         m_StreamingWorldScatterPrefab = entityRemapping[scatterPrefabClone.Index].Target;
-        UnityEngine.Assertions.Assert.AreEqual(m_StreamingWorld.EntityManager.GetBuffer<ProceduralScatterPrefab>(m_StreamingWorldScatterPrefab).Length, EntityManager.GetBuffer<ProceduralScatterPrefab>(scatterPrefab).Length);
+        Assert.AreEqual(m_StreamingWorld.EntityManager.GetBuffer<ProceduralScatterPrefab>(m_StreamingWorldScatterPrefab).Length, EntityManager.GetBuffer<ProceduralScatterPrefab>(scatterPrefab).Length);
             
         entityRemapping.Dispose();
-        EntityManager.DestroyEntity(m_ScatterPrefabQuery);
+        
+        m_StreamingWorld.EntityManager.RemoveComponent(m_StreamingWorld.EntityManager.UniversalGroup, typeof(TempProceduralScatterPrefabTag));
+
     }
 
     public void GenerateTileAsync(int3 tile, Entity scatterPrefab, NativeArray<ProceduralInstanceData> instance, JobHandle dependency)
     {
-        Debug.Log("Generate tile");
         PrepareScatterPrefab(scatterPrefab);
 
         var job = new GenerateTileJob();
@@ -133,7 +138,11 @@ class ScatterStreamingSystem : JobComponentSystem
         job.Schedule(dependency).Complete();
         
         m_StreamingWorld.EntityManager.EndExclusiveEntityTransaction();
-        EntityManager.MoveEntitiesFrom(m_StreamingWorld.EntityManager);
+        
+        var entityRemapping = m_StreamingWorld.EntityManager.CreateEntityRemapArray(Allocator.TempJob);
+        EntityManager.MoveEntitiesFrom(m_StreamingWorld.EntityManager, m_InstancesQuery, entityRemapping);
+        entityRemapping.Dispose();
+
     }
     
     public void UnloadTile(int3 tile)
@@ -147,18 +156,26 @@ class ScatterStreamingSystem : JobComponentSystem
     }
 
     protected override void OnCreateManager()
-    {
+    {       
+        m_StreamingWorld = new World("StreamingWorld");
+
         m_ScatterPrefabQuery = GetComponentGroup(new EntityArchetypeQuery 
         { 
-            All = new[] { ComponentType.ReadWrite<TempProceduralScatterPrefabTag>() }, 
+            All = new[] { ComponentType.ReadOnly<TempProceduralScatterPrefabTag>() }, 
             Options = EntityArchetypeQueryOptions.IncludePrefab | EntityArchetypeQueryOptions.IncludeDisabled
         });
+        m_InstancesQuery = m_StreamingWorld.EntityManager.CreateComponentGroup(new EntityArchetypeQuery 
+        { 
+            None = new[] { ComponentType.ReadOnly<ProceduralScatterPrefab>() },
+            Options = EntityArchetypeQueryOptions.IncludeDisabled
+        });
 
-        m_StreamingWorld = new World("StreamingWorld");
     }
 
     protected override void OnDestroyManager()
     {
+        m_InstancesQuery.Dispose();
+        m_StreamingWorld.Dispose();
     }
 }
 
@@ -169,37 +186,38 @@ class TrivialScatterSystem : JobComponentSystem
         RequireSingletonForUpdate<ProceduralScatterPrefab>();
     }
 
-    bool once = false;
+    int counter = 0;
     
 
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        if (once)
+        counter++;
+        if (counter >= 10)
+        {
             return inputDeps;
-
-        Debug.Log("Scatter");
-        once = true;
+        }
         
         var scatterSingletonQuery = GetComponentGroup(typeof(ProceduralScatterPrefab));
         var scatterSingleton = scatterSingletonQuery.GetSingletonEntity();
 
         var prefabs = EntityManager.GetBuffer<ProceduralScatterPrefab>(scatterSingleton);
         
-        var instanceArray = new NativeArray<ProceduralInstanceData>(1000, Allocator.TempJob);
+        var instanceArray = new NativeArray<ProceduralInstanceData>(512, Allocator.TempJob);
         var random = new Random(0x6E624EB7u);
         for (int i = 0; i != instanceArray.Length; i++)
         {
             var instance = new ProceduralInstanceData();
-            instance.Position = new float3(random.NextFloat(0, 20), 0, random.NextFloat(0, 20));
+            instance.Position = new float3(counter * 10 + random.NextFloat(0, 10), 0, random.NextFloat(0, 20));
             instance.Rotation = random.NextQuaternionRotation();
-            instance.Scale = 1.0F;
+            instance.Scale = 0.2F;
             instance.PrefabIndex = random.NextInt(0, prefabs.Length);
 
             instanceArray[i] = instance;
         }
         
-        World.GetExistingManager<ScatterStreamingSystem>().GenerateTileAsync(new int3(0, 0, 0), scatterSingleton, instanceArray, default(JobHandle));
+        World.GetExistingManager<ScatterStreamingSystem>().GenerateTileAsync(new int3(counter, 0, 0), scatterSingleton, instanceArray, default(JobHandle));
 
+        var blah = scatterSingletonQuery.GetSingletonEntity();
         
         return inputDeps;
     }
