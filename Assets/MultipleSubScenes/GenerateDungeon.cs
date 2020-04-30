aa60f14fd27a0b81d83b7ae35b5c5970b4d47f76
Scott Anderson <scott.anderson@unity3d.com>
2020-04-30 13:14:51 -0700

    Add a simple run-time dungeon editor, will not work in player builds due to RoomSet reliance on AssetDatabase, but it will allow for playable dungeons

---
+++ Assets/MultipleSubScenes/GenerateDungeon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(RoomSet))]
+public class GenerateDungeon : MonoBehaviour
+{
+    public DungeonTiles tiles;
+
+    private RoomSet roomSet;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        roomSet = GetComponent<RoomSet>();
+        roomSet.LoadTileSet(tiles.SubScenes);
+    }
+
+    void Start()
+    {
+        var guid = new GUID(AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(tiles.SubScenes[Random.Range(0,tiles.SubScenes.Count - 1)])));
+        roomSet.RequestGeneration(guid, tiles);
+    }
+
+    void OnDestroy()
+    {
+        roomSet.CleanupGeneration();
+    }
+}
